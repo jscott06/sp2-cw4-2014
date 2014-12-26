4307bc2184c6cd115792ee48f90b865ba01470c8
jackscotti <jacopojackscotti@gmail.com>
2014-12-26 19:00:17 +0100

    Add placeAt and isOccupied tests

--- Battleship/src/OceanTest.java
+++ Battleship/src/OceanTest.java
@@ -4,24 +4,48 @@ import org.junit.Test;
 
 
 public class OceanTest {
-//	@Test WON'T WORK UNTIL I'VE IMPLEMENTED placeShipAt
-//	public void testIsOccupied() {
-//		Ocean ocean = new Ocean();
-//		Destroyer destroyer = new Destroyer();
-//		Submarine submarine = new Submarine();
-//		Battleship battleship = new Battleship();
-//		Cruiser cruiser = new Cruiser();
-//		
-//		destroyer.placeShipAt(0, 1, true, ocean);
-//		submarine.placeShipAt(1, 0, true, ocean);
-//		battleship.placeShipAt(1, 1, true, ocean);
-//		cruiser.placeShipAt(1, 2, true, ocean);
-//		
-//		assertEquals(true, ocean.isOccupied(0,0));
-//		assertEquals(true, ocean.isOccupied(0,1));
-//		assertEquals(true, ocean.isOccupied(1,0));
-//		assertEquals(true, ocean.isOccupied(1,1));
-//		assertEquals(true, ocean.isOccupied(1,2));
-//		
-//	}
+	@Test
+	public void testIsOccupiedAndPlaceShipAt() {
+		Ocean ocean = new Ocean();
+		Submarine submarine = new Submarine();
+		Destroyer destroyer = new Destroyer();
+		Cruiser cruiser = new Cruiser();
+		Battleship battleship = new Battleship();
+		
+		assertEquals(false, ocean.isOccupied(0, 0)); // sea
+		assertEquals(false, ocean.isOccupied(1, 0)); // will be submarine H
+		assertEquals(false, ocean.isOccupied(5, 0)); // will be destroyer H
+		assertEquals(false, ocean.isOccupied(5, 1)); // will be destroyer H
+		assertEquals(false, ocean.isOccupied(5, 2)); // sea
+		assertEquals(false, ocean.isOccupied(2, 5)); // will be cruiser V
+		assertEquals(false, ocean.isOccupied(3, 5)); // will be cruiser V
+		assertEquals(false, ocean.isOccupied(4, 5)); // will be cruiser V
+		assertEquals(false, ocean.isOccupied(5, 5)); // sea
+		assertEquals(false, ocean.isOccupied(5, 7)); // will be battleship V
+		assertEquals(false, ocean.isOccupied(6, 7)); // will be battleship V
+		assertEquals(false, ocean.isOccupied(7, 7)); // will be battleship V
+		assertEquals(false, ocean.isOccupied(8, 7)); // will be battleship V
+		assertEquals(false, ocean.isOccupied(9, 7)); // sea
+		
+		submarine.placeShipAt(1, 0, true, ocean);
+		destroyer.placeShipAt(5, 0, true, ocean);
+		cruiser.placeShipAt(2, 5, false, ocean);
+		battleship.placeShipAt(5, 7, false, ocean);
+		
+		assertEquals(false, ocean.isOccupied(0, 0)); // sea
+		assertEquals(true, ocean.isOccupied(1, 0)); // submarine H
+		assertEquals(true, ocean.isOccupied(5, 0)); // destroyer H
+		assertEquals(true, ocean.isOccupied(5, 1)); // destroyer H
+		assertEquals(false, ocean.isOccupied(5, 2)); // sea
+		assertEquals(true, ocean.isOccupied(2, 5)); // cruiser V
+		assertEquals(true, ocean.isOccupied(3, 5)); // cruiser V
+		assertEquals(true, ocean.isOccupied(4, 5)); // cruiser V
+		assertEquals(false, ocean.isOccupied(5, 5)); // sea
+		assertEquals(true, ocean.isOccupied(5, 7)); // will be battleship V
+		assertEquals(true, ocean.isOccupied(6, 7)); // will be battleship V
+		assertEquals(true, ocean.isOccupied(7, 7)); // will be battleship V
+		assertEquals(true, ocean.isOccupied(8, 7)); // will be battleship V
+		assertEquals(false, ocean.isOccupied(9, 7)); // sea
+		
+	}
 }
--- Battleship/src/ShipTest.java
+++ Battleship/src/ShipTest.java
@@ -25,5 +25,31 @@ public class ShipTest {
 		assertEquals(1, submarine.getLength());
 		assertEquals(false, battleship.getHits()[0]);
 	}
+	
+	@Test
+	public void testOkToPlaceShipAt(){
+		
+	}
+	
+	@Test
+	public void testPlaceShipAt(){
+		
+		Ocean ocean = new Ocean();
+		Submarine submarine = new Submarine();
+		submarine.placeShipAt(0, 0, true, ocean);
+		assertEquals(0, submarine.getBowColumn());
+		assertEquals(0, submarine.getBowRow());
+		
+	}
+	
+	@Test
+	public void testHit(){
+		
+	}
+	
+	@Test
+	public void testIsSunk(){
+		
+	}
 
 }
